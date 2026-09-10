4c4d5efb9e550cd2b5e448ab99d2d0dd9a99cb46
prashantmahawar02 <prashantmahawar02@gmail.com>
2026-09-10 12:17:06 +0530

    Create SW.js

---
+++ SW.js
@@ -0,0 +1,11 @@
+```javascript
+const CACHE_NAME = 're-crm-v1';
+const ASSETS = ['./', './index.html'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
